43cf7a28d0a2872955ede74962def2d1abb0b84f
avan31017 <avan31017@gmail.com>
2024-06-26 00:52:43 +0900

    Initailized Project

---
+++ WeatherKit_with_AsyncAwait/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherKit_with_AsyncAwait
+//
+//  Created by 조성빈 on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherKit_with_AsyncAwait/WeatherKit_with_AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKit_with_AsyncAwaitApp.swift
+//  WeatherKit_with_AsyncAwait
+//
+//  Created by 조성빈 on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKit_with_AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
